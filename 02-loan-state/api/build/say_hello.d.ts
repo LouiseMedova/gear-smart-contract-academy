0fd08c1dbddf615c594c29470c38d7025d9ad46a
Louisa <luiza.medova@phystech.edu>
2022-06-03 14:02:32 +0300

    api

--- 02-loan-state/api/build/say_hello.d.ts
+++
@@ -1,2 +0,0 @@
-import { GearApi } from '@gear-js/api';
-export declare const sendMessage: (api: GearApi, destination: any, pathToMeta?: string, account?: any, value?: any, payload?: any) => Promise<void>;
